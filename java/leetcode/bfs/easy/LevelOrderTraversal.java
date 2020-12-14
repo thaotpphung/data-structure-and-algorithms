de693663c9558240a51edf0cfd2958637d3de5cf
Thao Phung <ttp7428@truman.edu>
2020-12-14 14:53:55 -0600

    removed DS_Store file

--- java/leetcode/bfs/easy/LevelOrderTraversal.java
+++ java/leetcode/bfs/easy/LevelOrderTraversal.java
@@ -1,4 +1,4 @@
-package BFS.easy;
+package bfs.easy;
 
 // Problem Statement #
 // Given a binary tree, populate an array to represent its level-by-level traversal. You should populate the values of all nodes of each level from left to right in separate sub-arrays.
